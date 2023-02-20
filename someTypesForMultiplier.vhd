b6f634505f1b6aa5edf56ab141554deb8eaa799d
frankandes <ffa7120@g.rit.edu>
2023-02-20 09:46:14 -0500

    added some multiplier help

---
+++ someTypesForMultiplier.vhd
@@ -0,0 +1,10 @@
+type carry_array is array(N-1 downto 1) of std_logic_vector(N-1 downto 0);
+type and_array is array(N-1 downto 0) of std_logic_vector(N-1 downto 0);
+.
+.
+.
+a : and_array
+.
+.
+.
+begin
